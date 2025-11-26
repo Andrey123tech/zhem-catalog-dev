0e5f46580caa42d4a181410e436d3c7ca307afc2
Andrey123tech <BigKZ12@gmail.com>
2025-11-26 22:38:27 +0500

    Update catalog_data.js

--- src/catalog_data.js
+++ src/catalog_data.js
@@ -9,4 +9,8 @@ for (let v = 15.0; v <= 23.5; v += 0.5) {
   SIZES.push(v.toFixed(1));
 }
 
-export { PRODUCTS, SIZES };
+// Размерная линейка браслетов
+const BRACELET_SIZES = ["17.0", "18.0", "19.0"];
+
+export { PRODUCTS, SIZES, BRACELET_SIZES };
+
